3d678bc11ebc67a9fbd667d177a48d5272e80109
francisco-1998 <francmarinc@gmail.com>
2021-03-04 10:57:45 -0500

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+` ;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
+`;
+
+function App() {
+  return (
+    <Contenedor>
+      <Boton>Obtener frase</Boton>
+    </Contenedor>
+  );
+}
+
+export default App;
